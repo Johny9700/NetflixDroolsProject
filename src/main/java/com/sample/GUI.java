82a7fc42c35f7746bc146694bcec0cd0749046ba
Unknown <darasg2008@wp.pl>
2018-12-09 14:14:37 +0100

    Add GUI class

---
+++ src/main/java/com/sample/GUI.java
@@ -0,0 +1,32 @@
+package com.sample;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class GUI {
+	public static String askWindow(JFrame frame, String question, ArrayList variants) {
+		Object[] options = {variants.get(0),
+                            variants.get(1)};
+                            
+		int response = JOptionPane.showOptionDialog(frame,
+                                             question,
+                                             "Netflix",
+                                             JOptionPane.YES_NO_OPTION,
+                                             JOptionPane.QUESTION_MESSAGE,
+                                             null,
+                                             options,
+                                             options[0]);
+		
+		switch (response) {
+    		case JOptionPane.YES_OPTION: return (String)variants.get(0);
+    		case JOptionPane.NO_OPTION: return (String)variants.get(1);
+    		default: return "";
+    	}
+	}
+
+	public static void recommendWindow(JFrame frame, String recommendation) {
+		int n = JOptionPane.showConfirmDialog(null,
+											recommendation,
+											"Recommendation",
+											JOptionPane.PLAIN_MESSAGE);
+	}
+}
