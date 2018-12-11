b10d0ed11bd668295528431bfca814ec07d3fa32
Unknown <darasg2008@wp.pl>
2018-12-11 09:24:09 +0100

    Minor improvements
    Moved welcome window displaying to main
    Unified indentation style
    A few more style improvements to get rid of warnings

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -3,20 +3,25 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import javax.swing.*;
 
 public class DroolsTest {
 
-	public static void main(String[] args) {
-		try {
-			// load up the knowledge base
-			KieServices ks = KieServices.Factory.get();
-			KieContainer kContainer = ks.getKieClasspathContainer();
-			KieSession kSession = kContainer.newKieSession("ksession-rules");
+    public static void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KieServices ks = KieServices.Factory.get();
+            KieContainer kContainer = ks.getKieClasspathContainer();
+            KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-			kSession.fireAllRules();
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
-	}
+            JFrame frame = new JFrame();
+            if (GUI.initWindow(frame)) {
+                kSession.fireAllRules();
+            }
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/com/sample/GUI.java
+++ src/main/java/com/sample/GUI.java
@@ -4,35 +4,38 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 public class GUI {
-	public static String askWindow(JFrame frame, String question, ArrayList variants) {
-		Object[] options = { variants.get(0), variants.get(1) };
+    public static String askWindow(JFrame frame, String question, ArrayList<String> variants) {
+        Object[] options = { variants.get(0), variants.get(1) };
 
-		int response = JOptionPane.showOptionDialog(frame, question, "Netflix", JOptionPane.YES_NO_OPTION,
-				JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        int response = JOptionPane.showOptionDialog(frame, question, "Netflix", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 
-		switch (response) {
-		case JOptionPane.YES_OPTION:
-			return (String) variants.get(0);
-		case JOptionPane.NO_OPTION:
-			return (String) variants.get(1);
-		default:
-			return "";
-		}
-	}
+        switch (response) {
+        case JOptionPane.YES_OPTION:
+            return (String) variants.get(0);
+        case JOptionPane.NO_OPTION:
+            return (String) variants.get(1);
+        default:
+            return "";
+        }
+    }
 
-	public static void recommendWindow(JFrame frame, String recommendation) {
-		ImageIcon icon = new ImageIcon("src/main/resources/img/film_icon_48.png");
-		int n = JOptionPane.showConfirmDialog(null, recommendation, "Recommendation", JOptionPane.DEFAULT_OPTION,
-				JOptionPane.PLAIN_MESSAGE, icon);
-	}
+    public static void recommendWindow(JFrame frame, String recommendation) {
+        ImageIcon icon = new ImageIcon("src/main/resources/img/film_icon_48.png");
+        JOptionPane.showConfirmDialog(null, recommendation, "Recommendation", JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE, icon);
+    }
 
-	public static void initWindow(JFrame frame) {
-		ImageIcon icon = new ImageIcon();
-		int n = JOptionPane.showConfirmDialog(null,
-				"Welcome to the program which can help you with dealing with the problem: \n"
-						+ "                             \"What should I watch today on Netflix?\"\n "
-						+ "       Just answer the questions and the program will help you to choose.\n"
-						+ "               Inf132212 - Jan Dudziak                  Inf132235 - Dariusz Grynia",
-				"Netflix", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, icon);
-	}
+    public static boolean initWindow(JFrame frame) {
+        ImageIcon icon = new ImageIcon();
+        int response = JOptionPane.showConfirmDialog(null,
+                "Welcome to the program which will help you dealing with the problem: \n"
+                        + "                             \"What should I watch today on Netflix?\"\n "
+                        + "       Just answer the questions and the program will help you to choose.\n" + "\n"
+                        + "               Inf132212 - Jan Dudziak                  Inf132235 - Dariusz Grynia",
+                "Netflix", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, icon);
+        if (response == JOptionPane.CLOSED_OPTION)
+            return false;
+        return true;
+    }
 }
